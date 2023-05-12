42401b7d6f0f06b7ff049435dbe54bcd83d39711
Sylwia <stefansylwia@interia.pl>
2023-05-12 19:18:40 +0200

    feat: creating Error Middleware

---
+++ src/middlewares/errorMiddleware.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+
+
+function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+    const statusCode = res.statusCode ? res.statusCode : 500;
+    
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'development' ? err.stack : null,
+    });
+};
+
+export default errorHandler;
