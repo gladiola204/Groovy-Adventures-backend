f5f182069caf00664c75c6450a20568f5a7e744f
Sylwia <stefansylwia@interia.pl>
2023-05-15 19:02:15 +0200

    feat: add login, logout, resendEmail functionality

--- src/users/usersController.ts
+++ src/users/usersController.ts
@@ -1,22 +1,22 @@
 import { Request, Response } from "express"
 import User from "../models/userModel";
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import { Types } from 'mongoose';
 import Token from "../models/tokenModel";
 import sendEmail from "../utils/sendEmail";
-import crypto from 'crypto';
+import bcryptjs from 'bcryptjs';
+import IUser from "../types/user.interface";
 
 const isValidEmail = (email: string) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
     return emailRegex.test(email);
 }
 
 const validation = (res: Response, body: {email: string, login:string, password: string}) => {
     const { email, login, password } = body;
-    if(!body || !email || !login || !password) {
-        res.status(400);
-        throw new Error("Please fill in all required fields");
-    };
+
+    areDataExist(res, [body, email, login, password], "Please fill in all required fields", true);
+
     if(email.trim() === '' || login.trim() === '' || password.trim() === '') {
         res.status(400);
         throw new Error("Please fill in all required fields");
@@ -24,49 +24,29 @@ const validation = (res: Response, body: {email: string, login:string, password:
     if(password.length < 6) {
         res.status(400);
         throw new Error("Password must be more than 6 characters");
-    }
-    if(isValidEmail(email) === false) {
+    };
+
+    if(!isValidEmail(email)) {
         res.status(400);
-        throw new Error("Password must be more than 6 characters");
+        throw new Error("Invalid email address format.");
     }
+};
+
+export const areDataExist = (res: Response, data: Array<Object | string | boolean | null>, message: string, shouldExist: boolean) => {
+    const foundData = data.every(item => item !== null && item !== undefined);
+
+    if ((foundData && !shouldExist) || (!foundData && shouldExist)) {
+        res.status(400);
+        throw new Error(message);
+  }
 }
 
 const generateToken = (id: Types.ObjectId) => {
     return jwt.sign({id}, `${process.env.JWT_SECRET}`, {expiresIn: "1d"});
 }
 
-export async function registerUser(req: Request, res: Response) {
-
-    const { body } = req;
-    const { email, login, password } = body;
-
-    validation(res, body);
-
-    // Check email in database
-    const userExists = await User.findOne({email});
-
-    if(userExists) {
-        res.status(400);
-        throw new Error("Email has already been registered.")
-    };
-
-    // Check login in database 
-    const loginExists = await User.findOne({login});
-
-    if(loginExists) {
-        res.status(400);
-        throw new Error("Login is already taken.")
-    };
-
-    // Create user
-    const user = await User.create({
-        email,
-        login,
-        password
-    });
-
-    if(user) {
-        const { email, login, _id } = user;
+const verificationTokenAndEmail = async (req: Request, res: Response, user: IUser) => {
+    const { email, login, _id } = user;
 
         // Generate confirm email token 
         const confirmEmailToken = generateToken(_id);
@@ -103,13 +83,39 @@ export async function registerUser(req: Request, res: Response) {
             await sendEmail(subject, message, send_to, sent_from);
             res.status(200).json({
                 success: true,
-                message: "Reset email sent"
+                message: "Confirmation email sent"
             })
         } catch {
             res.status(500);
             throw new Error("Email not sent, please try again")
         };
+}
 
+export async function registerUser(req: Request, res: Response) {
+    const { body } = req;
+    const { email, login, password } = body;
+
+    validation(res, body);
+
+    // Check email in database
+    const userExists = await User.findOne({email});
+    
+    areDataExist(res, [userExists], "Email has already been registered.", false);
+
+    // Check login in database 
+    const loginExists = await User.findOne({login});
+
+    areDataExist(res, [loginExists], "Login is already taken", false);
+
+    // Create user
+    const user = await User.create({
+        email,
+        login,
+        password
+    });
+
+    if(user) {
+        verificationTokenAndEmail(req, res, user);
     } else {
         res.status(400);
         throw new Error("Invalid user data");
@@ -119,6 +125,14 @@ export async function registerUser(req: Request, res: Response) {
 
 export async function confirmEmail(req: Request, res: Response) {
     const { token } = req.params;
+
+    // Verify token
+    const verified = jwt.verify(token, `${process.env.JWT_SECRET}`) as JwtPayload;
+
+    if(!verified) {
+        res.status(401);
+        throw new Error('Token is not verified.');
+    };
 
     const validToken = await Token.findOne({ 
         token,
@@ -140,3 +154,91 @@ export async function confirmEmail(req: Request, res: Response) {
         throw new Error("Invalid or expired token")
     }
 }
+
+export async function resendVerificationEmail(req: Request, res: Response) {
+    
+    const { body } = req;
+    const { email } = body;
+
+    areDataExist(res, [body, email], "Please fill in email field", true);
+
+    if(!isValidEmail(email)) {
+        res.status(400);
+        throw new Error("Invalid email address format.");
+    }
+
+    const userExists = await User.findOne({ email });
+
+    if(userExists && userExists.emailVerified === false) {
+        verificationTokenAndEmail(req, res, userExists);
+
+    } else if (userExists && userExists?.emailVerified === true) {
+        res.status(400);
+        throw new Error("Email has been already verified."); 
+    } else {
+        res.status(400);
+        throw new Error("User doesn't exist.")
+    }
+};
+
+export async function loginUser(req: Request, res: Response) {
+    const { body } = req;
+    const { email, password } = body;
+
+    areDataExist(res, [body, email, password], "Please fill in all required fields", true);
+
+    if(password.length < 6) {
+        res.status(400);
+        throw new Error("Invalid password or email");
+    };
+
+    if(!isValidEmail(email)) {
+        res.status(400);
+        throw new Error("Invalid email address format.");
+    };
+
+    const userExists: IUser | null = await User.findOne({ email });
+
+    if(userExists === null) {
+        res.status(400);
+        throw new Error("User doesn't exist. Please sign up.")
+    }
+    const { _id, emailVerified, login, phone } = userExists;
+
+    // Check if password is correct
+    const isPasswordCorrect = await bcryptjs.compare(password, userExists.password);
+    if(isPasswordCorrect === false) {
+        res.status(400);
+        throw new Error("Invalid email or password.");
+    }
+
+    if(emailVerified === false) {
+        res.status(401);
+        throw new Error("Email has not yet been verified. Verify it to be able to log in")
+    };
+
+    const token = generateToken(_id);
+
+    // Save token to DB
+    await new Token({
+        userId: _id,
+        token,
+        expiresAt: Date.now() + (24 * 60 * 60 * 1000), // 24 hours
+    }).save();
+
+    res.status(200).json({
+        _id, login, email, phone, token,
+    });
+};
+
+export async function logoutUser(req: Request, res: Response) {
+    const { token } = req.body;
+    console.log("Jestem tutaj");
+
+    await Token.deleteOne({ token });
+
+    res.status(200).json({
+        success: true,
+        message: "User logged out."
+    })
+};
--- src/users/usersRouter.ts
+++ src/users/usersRouter.ts
@@ -1,10 +1,14 @@
 import express from 'express';
-import { confirmEmail, registerUser } from './usersController';
+import { confirmEmail, loginUser, logoutUser, registerUser, resendVerificationEmail } from './usersController';
 import { catchAsync } from '../utils/catchAsync';
+import { protection } from '../middlewares/authMiddleware';
 
 const usersRouter = express.Router();
 
 usersRouter.post('/register', catchAsync(registerUser));
 usersRouter.patch('/confirmemail/:token', catchAsync(confirmEmail));
+usersRouter.post('/resendverificationemail', catchAsync(resendVerificationEmail));
+usersRouter.post('/login', catchAsync(loginUser));
+usersRouter.delete('/logout', catchAsync(protection), catchAsync(logoutUser));
 
 export default usersRouter;
