296a65ea3aea1f5cc5e50f9d6485ee3718b97544
Sylwia <stefansylwia@interia.pl>
2023-05-16 10:52:29 +0200

    feat: add change password route and functionality

--- src/users/usersController.ts
+++ src/users/usersController.ts
@@ -270,3 +270,40 @@ export async function updateUser(req: Request, res: Response) {
         throw new Error("User not found");
     };
 };
+
+export async function changePassword(req: Request, res: Response) {
+    const { oldPassword, newPassword } = req.body;
+
+    areDataExist(res, [oldPassword, newPassword], "Please fill in all required fields", true);
+
+    if(newPassword.length < 6) {
+        res.status(400);
+        throw new Error("New password must be more than 6 characters.");
+    };
+    if(oldPassword.length < 6) {
+        res.status(400);
+        throw new Error("Invalid old password");
+    }
+
+    const user = await User.findById(req.user?._id);
+
+    if(user === null) {
+        res.status(404);
+        throw new Error("User not found");
+    };
+
+    const isPasswordCorrect = await bcryptjs.compare(oldPassword, user.password);
+
+    if(isPasswordCorrect === false) {
+        res.status(400);
+        throw new Error("Invalid old password");
+    };
+
+    user.password = newPassword;
+    await user.save();
+
+    res.status(200).json({
+        success: true,
+        message: "Password has been changed successfully."
+    });
+}
--- src/users/usersRouter.ts
+++ src/users/usersRouter.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { confirmEmail, loginUser, logoutUser, registerUser, resendVerificationEmail, statusUser, updateUser } from './usersController';
+import { changePassword, confirmEmail, loginUser, logoutUser, registerUser, resendVerificationEmail, statusUser, updateUser } from './usersController';
 import { catchAsync } from '../utils/catchAsync';
 import { protection } from '../middlewares/authMiddleware';
 
@@ -12,5 +12,7 @@ usersRouter.post('/login', catchAsync(loginUser));
 usersRouter.delete('/logout', catchAsync(protection), catchAsync(logoutUser));
 usersRouter.get('/loggedin', catchAsync(protection), catchAsync(statusUser));
 usersRouter.patch('/updateuser', catchAsync(protection), catchAsync(updateUser));
+usersRouter.patch('/changepassword', catchAsync(protection), catchAsync(changePassword));
+// usersRouter.post('/forgotpassword', catchAsync(protection), catchAsync(changePassword));
 
 export default usersRouter;
