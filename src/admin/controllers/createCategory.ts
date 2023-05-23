54eaa6fce3f06fab30597c539d54c778b3fb47c5
Sylwia <stefansylwia@interia.pl>
2023-05-23 09:04:43 +0200

    refactor: add icon to DB

--- src/admin/controllers/createCategory.ts
+++ src/admin/controllers/createCategory.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
 import checkDataExistence from "../../utils/validators/checkDataExistence";
 import Category from "../../models/categoryModel";
+import uploadImages from "./utils/uploadImages";
 
-async function createTour(req: Request, res: Response) {
+async function createCategory(req: Request, res: Response) {
     const { title } = req.body;
 
-    checkDataExistence(res, [req.body, title], "Please fill in all fields", true);
+    checkDataExistence(res, [req.body, title, req.file], "Please fill in all fields", true);
 
     const categoryDB = await Category.findOne({ title });
     if(categoryDB !== null) {
@@ -13,11 +14,14 @@ async function createTour(req: Request, res: Response) {
         throw new Error("Category already exists")
     };
 
+    const uploadedIcon = await uploadImages(req, res);
+
     const category = await new Category({
-        title
+        title,
+        icon: uploadedIcon[0].fileData
     }).save();
 
     res.status(201).json(category);
 }
 
-export default createTour;
+export default createCategory;
