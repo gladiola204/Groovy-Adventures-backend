a59a60d270b9786fed0b50793534f3c965cde881
Sylwia <stefansylwia@interia.pl>
2023-05-23 09:02:28 +0200

    feat: add slug and exact-typing icon

--- src/models/categoryModel.ts
+++ src/models/categoryModel.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose"
+import updateSlug from "../utils/updateSlug";
 
 const categorySchema = new mongoose.Schema({
     title: {
@@ -8,10 +9,27 @@ const categorySchema = new mongoose.Schema({
         unique: true,
     },
     icon: {
-        type: Object,
-        default: {},
-        //required: [true, 'Please add an icon']
+        type: {
+            fileName: {type: String, required: [true, 'Please add a file name']},
+            filePath: {type: String, required: [true, 'Please add a file path']},
+            filePublicId: {type: String, required: [true, 'Please add a file public ID']},
+            fileType: {type: String, required: [true, 'Please add a file type']},
+            fileSize: {type: String, required: [true, 'Please add a file size']},
+        },
+        required: [true, 'Please add an icon']
     },
+    slug: {
+        type: String, 
+        unique: true
+    }
+});
+
+categorySchema.pre('save', async function (next: (err?: Error) => void) {
+    if (this.isModified('title')) {
+        this.slug = updateSlug(this.title);
+    }
+
+    next();
 });
 
 const Category = mongoose.model('Category', categorySchema);
